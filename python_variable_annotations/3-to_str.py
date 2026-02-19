3e7ed99cc1ada79670c2f438da7cb8fa1907ac08
saraum1 <saaraaaallls1@gmail.com>
2026-02-19 11:33:42 +0300

    Create 3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that provides a type-annotated to_str function."""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float."""
+    return str(n)
