bbf0aaee03e5b052c169ac599b49bc376b8d7b5b
saraum1 <saaraaaallls1@gmail.com>
2026-03-14 23:50:10 +0300

    Update 2-hbtn_course.js

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,6 +1,6 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    // Verifica el tipo de los atributos durante la creación del objeto
+  
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
@@ -11,18 +11,18 @@ class HolbertonCourse {
       throw new TypeError('Students must be an array of strings');
     }
 
-    // Almacena los atributos en versiones con guion bajo
+    
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  // Implementa un getter para el atributo 'name'
+  
   get name() {
     return this._name;
   }
 
-  // Implementa un setter para el atributo 'name'
+
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
@@ -30,12 +30,12 @@ class HolbertonCourse {
     this._name = newName;
   }
 
-  // Implementa un getter para el atributo 'length'
+  
   get length() {
     return this._length;
   }
 
-  // Implementa un setter para el atributo 'length'
+  
   set length(newLength) {
     if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
@@ -43,12 +43,12 @@ class HolbertonCourse {
     this._length = newLength;
   }
 
-  // Implementa un getter para el atributo 'students'
+  
   get students() {
     return this._students;
   }
 
-  // Implementa un setter para el atributo 'students'
+  
   set students(newStudents) {
     if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
