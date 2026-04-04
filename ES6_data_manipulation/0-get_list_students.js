cbe0ba864ebc86dff5c523e411af24e63933a6a0
saraum1 <saaraaaallls1@gmail.com>
2026-04-04 17:11:41 +0300

    Create 0-get_list_students.js

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * Retrieves a list of student objects.
+ * @returns {Array} An array of objects containing id, firstName, and location.
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
